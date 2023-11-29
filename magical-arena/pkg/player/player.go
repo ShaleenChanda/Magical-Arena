689e4696c301db6706ae6475c1b1aeeb6a13e4af
ShaleenChanda <84751431+ShaleenChanda@users.noreply.github.com>
2023-11-30 03:57:07 +0530

    Player Struct Completed and Unit Test added for Player Struct functionality check

--- magical-arena/pkg/player/player.go
+++ magical-arena/pkg/player/player.go
@@ -1,20 +1,16 @@
 package player
 
-import "fmt"
 
-// Player represents a player in the game. It has attributes for health, strength, attack, wins, losses and draws.
+// Player represents a player in the game. It has attributes for health, strength and attack.
 type Player struct {
 	health int
 	strength int
 	attack int
-	wins int
-	losses int
-	draws int
 }
 
 
 // NewPlayer creates and initializes a new Player instance with the given attributes.
-// The function takes values for health, strength, and attack, and initializes wins and losses to 0.
+// The function takes values for health, strength, and attack.
 //
 // Parameters:
 //   - health: The health attribute of the player.
@@ -27,10 +23,8 @@ type Player struct {
 // Example:
 //   player := NewPlayer(100, 10, 5)
 //   fmt.Printf("Player has %d health, %d strength, and %d attack\n", player.health, player.strength, player.attack)
-//
-// Note: The wins and losses attributes are initialized to 0 by default.
 func NewPlayer(health, strength, attack int) *Player {
-	return &Player{health, strength, attack, 0, 0, 0}
+	return &Player{health, strength, attack}
 }
 
 
@@ -49,59 +43,7 @@ func NewPlayer(health, strength, attack int) *Player {
 //   fmt.Printf("Player has %d health, %d strength, and %d attack\n", health, strength, attack)
 //
 // Note: This function is designed for retrieving the core attributes of a player.
-func GetPlayerAttributes(p *Player) (int, int, int) {
+func GetPlayerBaseAttributes(p *Player) (int, int, int) {
 	return p.health, p.strength, p.attack
 }
 
-
-// GetPlayerMatchStats returns the match-related statistics of a player, including the number of wins, losses, draws, and total matches played.
-//
-// Parameters:
-//   - p: A pointer to the Player whose match-related stats are to be retrieved.
-//
-// Returns:
-//   - int: Number of wins for the player.
-//   - int: Number of losses for the player.
-//   - int: Number of draws for the player.
-//   - int: Total number of matches played by the player.
-//
-// Example:
-//   wins, losses, draws, totalMatchesPlayed := GetPlayerMatchStats(player)
-//   fmt.Printf("Player has %d wins, %d losses, %d draws, and has played %d matches\n", wins, losses, draws, totalMatchesPlayed)
-//
-// Note: This function is designed for retrieving the match-related statistics of a player.
-func GetPlayerMatchStats(p *Player) (int, int, int, int) {
-    totalMatchesPlayed := p.wins + p.losses + p.draws
-    return p.wins, p.losses, p.draws, totalMatchesPlayed
-}
-
-
-// IncrementStat increments the specified attribute (e.g., wins, losses, draws) of a player and returns the updated value.
-//
-// Parameters:
-//   - p: A pointer to the Player whose attribute is to be incremented.
-//   - stat: The attribute (wins, losses, draws) to be incremented.
-//
-// Returns:
-//   - int: The updated value of the specified attribute for the player.
-//   - error: An error value if the attribute name is invalid.
-//
-// Example:
-//   wins, err := IncrementStat(player, "wins")
-//   losses, err := IncrementStat(player, "losses")
-//   draws, err := IncrementStat(player, "draws")
-func IncrementStat(p *Player, stat string) (int, error) {
-    switch stat {
-    case "wins":
-        p.wins++
-        return p.wins, nil
-    case "losses":
-        p.losses++
-        return p.losses, nil
-    case "draws":
-        p.draws++
-        return p.draws, nil
-    default:
-        return 0, fmt.Errorf("Invalid stat: %s", stat)
-    }
-}
--- magical-arena/pkg/player/player_test.go
+++ magical-arena/pkg/player/player_test.go
@@ -1,3 +1,69 @@
 package player
 
-import 
+import (
+	"fmt"
+	"os"
+	"testing"
+)
+
+
+func TestNewPlayer(t *testing.T) {
+	//testing NewPlayer and GetPlayerBaseAttributes
+	//TEST 1: create a player with health 100, strength 10, attack 5, and check the assignment of values is correct
+	player := NewPlayer(100, 10, 5)
+	health, strength, attack := GetPlayerBaseAttributes(player)
+	if health != 100 {
+		t.Errorf("Expected player.health to be 100, got %d", health)
+	}
+	if strength != 10 {
+		t.Errorf("Expected player.strength to be 10, got %d", strength)
+	}
+	if attack != 5 {
+		t.Errorf("Expected player.attack to be 5, got %d", attack)
+	}
+
+	//TEST 2: create a playerA with health 50, strength 5, attack 2
+	//		create a playerB with health 100, strength 10, attack 5, and check the assignment of values is correct
+	playerA := NewPlayer(50, 5, 2)
+	playerB := NewPlayer(100, 10, 5)
+	healthA, strengthA, attackA := GetPlayerBaseAttributes(playerA)
+	healthB, strengthB, attackB := GetPlayerBaseAttributes(playerB)
+	//check playerA
+	if healthA != 50 {
+		t.Errorf("Expected playerA.health to be 50, got %d", healthA)
+	}
+	if strengthA != 5 {
+		t.Errorf("Expected playerA.strength to be 5, got %d", strengthA)
+	}
+	if attackA != 2 {
+		t.Errorf("Expected playerA.attack to be 2, got %d", attackA)
+	}
+	//check playerB
+	if healthB != 100 {
+		t.Errorf("Expected playerB.health to be 100, got %d", healthB)
+	}
+	if strengthB != 10 {
+		t.Errorf("Expected playerB.strength to be 10, got %d", strengthB)
+	}
+	if attackB != 5 {
+		t.Errorf("Expected playerB.attack to be 5, got %d", attackB)
+	}
+
+	//TEST 3: create a playerA with health 0, strength 0, attack 0
+	//create a playerB with health 0, strength 0, attack 0, and check that they are equal. 
+	playerA = NewPlayer(0, 0, 0)
+	playerB = NewPlayer(0, 0, 0)
+	healthA, strengthA, attackA = GetPlayerBaseAttributes(playerA)
+	healthB, strengthB, attackB = GetPlayerBaseAttributes(playerB)
+	if(healthA != healthB || strengthA != strengthB || attackA != attackB) {
+		t.Errorf("Expected playerA and playerB to be equal, got %d %d %d and %d %d %d", healthA, strengthA, attackA, healthB, strengthB, attackB)
+	}
+}
+
+// TestMain runs the main testing suite.
+func TestMain(m *testing.M) {
+	fmt.Println("Testing player package...")
+	Result:=m.Run()
+	fmt.Println("Testing complete.")
+	os.Exit(Result)
+}
